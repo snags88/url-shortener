a63c5d5d1931a632ee64b141d3cfadb966e2889c
Seiji Naganuma <Snaganumas@gmail.com>
2015-04-25 12:53:45 -0400

    create valid-invalid tests for url and pass tests

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,10 @@
 class Url < ActiveRecord::Base
-
+  before_create :shorten_url
   validates :original, presence: true
+  validates :shortened, uniqueness: true
 
-
+  def shorten_url
+    self.shortened = self.original
+  end
 
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,9 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
-  describe 'new url' do
-    let(:url){create(:url)}
+  context 'valid url' do
+    let!(:url){create(:url)}
 
+    it 'has a valid factory' do
+      expect(url).to be_valid
+    end
+    it 'defaults view count to 0' do
+      expect(url.views).to eq(0)
+    end
+    it 'has a shortened url' do
+      expect(url.shortened).not_to be_nil
+    end
 
   end
+
+  context 'invalid url' do
+    it 'is invalid without a url' do
+      url = build(:url, original: nil)
+      expect(url).not_to be_valid
+    end
+
+    it 'is invalid without a unqiue shortened url' do
+      url  = create(:url)
+      url2 = build(:url, shortened: url.shortened)
+      expect(url2).not_to be_valid
+    end
+  end
+
+  describe '#shorten_url' do
+    it 'creates the shortest url'
+    it 'creates a unique url'
+  end
+
 end
