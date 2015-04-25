b9a14f0729bbb79b92e8e2466e994be738cf49d9
Seiji Naganuma <Snaganumas@gmail.com>
2015-04-25 13:32:54 -0400

    create shorten_url method

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,8 +3,12 @@ class Url < ActiveRecord::Base
   validates :original, presence: true
   validates :shortened, uniqueness: true
 
-  def shorten_url
-    self.shortened = self.original
-  end
 
+  private
+    def shorten_url
+      begin
+        url = SecureRandom.urlsafe_base64(5) #=> handles a little bit over 1 billion unqiue urls
+      end while Url.where(:shortened => url).exists?
+      self.shortened = url
+    end
 end
